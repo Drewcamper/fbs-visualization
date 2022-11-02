69ac45e857494dd4293e9a1665a9755c704a072f
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2022-11-02 18:51:47 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ArrayGenerator from './components/arrayGenerator';
+
+function App() {
+  return (
+    <div>
+      <ArrayGenerator />
+    </div>
+  );
+}
+
+export default App;
